94b860170866dbd572e2cd716cb4c05503a4079c
Michal Danco <devdanco@gmail.com>
2019-08-05 18:53:27 +0200

    Add react router and create Home, Overview components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import Home from "./components/Home";
+import Overview from "./components/Overview";
 
 function App() {
-  return <div className="App">app</div>;
+  return (
+    <>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/overview" component={Overview} />
+      </Switch>
+    </>
+  );
 }
 
 export default App;
